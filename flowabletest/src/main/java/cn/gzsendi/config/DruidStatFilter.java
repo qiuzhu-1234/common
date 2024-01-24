9fb269b86eb0c69ca55c4080cc991b66d7454eb5
qiuzhu <1275286075@qq.com>
2024-01-24 22:49:54 +0800

    工作流程系统能稳定V3版本（各细节已修改）

--- flowabletest/src/main/java/cn/gzsendi/config/DruidStatFilter.java
+++ flowabletest/src/main/java/cn/gzsendi/config/DruidStatFilter.java
@@ -5,6 +5,9 @@ import javax.servlet.annotation.WebInitParam;
 
 import com.alibaba.druid.support.http.WebStatFilter;
 
+/**
+ *
+ */
 //注意不要忘记在 SpringBootSampleApplication.java 上添加 @ServletComponentScan 注解，不然就是404了。
 @WebFilter(
 		filterName = "druidWebStatFilter", urlPatterns = "/*", 
